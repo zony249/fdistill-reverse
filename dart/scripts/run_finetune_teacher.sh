52f95a1ed753058d8703051e7d3bfb7e5a090092
Zony Yu <zony249@gmail.com>
2024-06-17 15:50:23 -0600

    dart setup

--- dart/scripts/run_finetune_teacher.sh
+++ dart/scripts/run_finetune_teacher.sh
@@ -1,4 +1,22 @@
-python3 ../finetune.py \
+#!/bin/bash 
+
+#SBATCH --cpus-per-task=4 # number of cores
+#SBATCH --mem=32000 # 100M for the whole job 
+#SBATCH --time=3-00:00 # walltime in d-hh:mm or hh:mm:ss format
+#SBATCH --account=def-lilimou 
+#SBATCH --gres=gpu:1 # GPUs per node
+#SBATCH --output=slurm-logs/slurm-%j-dart-teacher.out
+
+nvidia-smi
+
+
+export MODEL_NAME=facebook/bart-large
+export EXP_NAME=$(date +%m-%d-%y--%T)--dart-teacher
+export MODEL_OUTPUT_PATH=runs/$EXP_NAME
+
+mkdir runs
+
+python3 finetune.py \
     --learning_rate=1e-5 \
     --gpus 1 \
     --do_train \
@@ -9,8 +27,8 @@ python3 ../finetune.py \
     --train_batch_size=8 --eval_batch_size=4 --gradient_accumulation_steps=1 \
     --max_source_length 128 --max_target_length=256 --val_max_target_length=256 --test_max_target_length=256\
     --val_check_interval 0.5 --eval_beams 5\
-    --data_dir '' \
-    --model_name_or_path '' \
+    --data_dir dart \
+    --model_name_or_path $MODEL_NAME \
     --output_dir $MODEL_OUTPUT_PATH \
     --overwrite_output_dir\
     "$@"
