11144ba18374bf6005411e0ccf2ccd63fe6bebb0
Zony Yu <zony249@gmail.com>
2024-06-25 13:54:06 -0400

    added dart scripts

--- dart/scripts/run_finetune_teacher.sh
+++ dart/scripts/run_finetune_teacher.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=4 # number of cores
 #SBATCH --mem=32000 # 100M for the whole job 
 #SBATCH --time=3-00:00 # walltime in d-hh:mm or hh:mm:ss format
-#SBATCH --account=def-lilimou 
+#SBATCH --account=rrg-lilimou 
 #SBATCH --gres=gpu:1 # GPUs per node
 #SBATCH --output=slurm-logs/slurm-%j-dart-teacher.out
 
--- dart/scripts/run_student_mle_reverse.sh
+++ dart/scripts/run_student_mle_reverse.sh
@@ -5,12 +5,12 @@
 #SBATCH --time=3-00:00 # walltime in d-hh:mm or hh:mm:ss format
 #SBATCH --account=def-lilimou 
 #SBATCH --gres=gpu:1 # GPUs per node
-#SBATCH --output=slurm-logs/slurm-%j-student-mle.out
+#SBATCH --output=slurm-logs/slurm-%j-student-mle-reverse.out
 
 nvidia-smi
 
 export TEACHER=models/dart-teacher-regularized/best_tfmr
-export EXP_NAME=student-mle-$(date +%m-%d-%y--%T)
+export EXP_NAME=student-mle-reverse-$(date +%m-%d-%y--%T)
 export OUTPUT=runs/$EXP_NAME
 
 
@@ -26,10 +26,11 @@ python3 distillation.py \
   --val_check_interval 0.3 --n_val -1 --eval_beams 5 --length_penalty=1. \
   --max_source_length=128 --max_target_length=128 --val_max_target_length=128 --test_max_target_length=128 \
   --model_name_or_path IGNORED \
-  --alpha_hid=0. --alpha_mlm=1. --alpha_ce=0. \
+  --alpha_hid=3. --alpha_mlm=1. --alpha_ce=0. \
   --train_batch_size=8 --eval_batch_size=8 --gradient_accumulation_steps=1 \
   --num_train_epochs=18 \
   --warmup_steps 100\
   --output_dir $OUTPUT\
   --overwrite_output_dir\
+  --reverse \
   "$@"
