870933af7b16ac912ec0e584014a5feae3d21e8d
Zony Yu <zony249@gmail.com>
2024-06-26 11:55:46 -0400

    finetune scripts for classification tasks

---
+++ classification/scripts/finetune/sst2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=4 # number of cores
+#SBATCH --mem=32000 # 100M for the whole job 
+#SBATCH --time=7-00:00 # walltime in d-hh:mm or hh:mm:ss format
+#SBATCH --account=def-lilimou 
+#SBATCH --gres=gpu:1 # GPUs per node
+#SBATCH --output=slurm-logs/slurm-%j-finetune-teacher-sst2.out
+
+nvidia-smi
+
+
+export TASK_NAME=sst2
+export EXP_NAME=$(date +%m-%d-%y--%T)--$TASK_NAME-teacher
+export OUTPUT=runs/$EXP_NAME
+
+python run_glue.py \
+  --model_name_or_path bert-base-uncased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir $OUTPUT
