c32485a9f4433ce6e4571d5a13b300b69c719ed7
Zony Yu <zony249@gmail.com>
2024-07-25 11:24:56 -0600

    model download also downloads the architectures for MNLI and STSB

--- classification/model-download-for-cc.py
+++ classification/model-download-for-cc.py
@@ -1,14 +1,42 @@
 from transformers import MBartForConditionalGeneration
 from transformers import T5ForConditionalGeneration
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig
+from copy import deepcopy
+import torch 
+from torch import nn
 
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
 tok = AutoTokenizer.from_pretrained("bert-base-uncased")
 
+config_mnli = AutoConfig.from_pretrained(
+    "bert-base-uncased", 
+    num_labels=3,
+    finetuning_task="mnli",
+)
+config_stsb = AutoConfig.from_pretrained(
+    "bert-base-uncased", 
+    num_labels=1,
+    finetuning_task="stsb",
+)
+
+
+model_mnli= AutoModelForSequenceClassification.from_pretrained(
+    "bert-base-uncased", 
+    config=config_mnli,
+)
+model_stsb= AutoModelForSequenceClassification.from_pretrained(
+    "bert-base-uncased", 
+    config=config_stsb,
+)
+
 model.save_pretrained("bert-base-uncased")
 tok.save_pretrained("bert-base-uncased")
 
+model_mnli.save_pretrained("bert-base-uncased-mnli")
+tok.save_pretrained("bert-base-uncased-mnli")
 
+model_stsb.save_pretrained("bert-base-uncased-stsb")
+tok.save_pretrained("bert-base-uncased-stsb")
 # model = T5ForConditionalGeneration.from_pretrained("t5-base")
 # tok = AutoTokenizer.from_pretrained("t5-base")
 
