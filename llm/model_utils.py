946f5d219afa8ab7d10ba70ae0617333ce512ee1
Zony Yu <zony249@gmail.com>
2025-08-07 15:59:46 +0000

    added support for Llama3

--- llm/model_utils.py
+++ llm/model_utils.py
@@ -10,9 +10,11 @@ from transformers import (
     AutoConfig, 
 )
 from transformers.models.qwen3 import Qwen3Config, Qwen3ForCausalLM
+from transformers.models.llama import LlamaConfig, LlamaForCausalLM
 
 LAYER_COPY_MAP = { 
     28: [0, 4, 9, 13, 18, 22, 27], 
+    32: [0, 4, 9, 13, 18, 22, 27, 31], 
     36: [0, 4, 9, 13, 18, 22, 27, 31, 35], 
     40: [0, 4, 9, 13, 18, 22, 27, 31, 35, 39]
 }
@@ -29,7 +31,7 @@ def load_model(hf_name_or_path: str,
 
 
 def create_student_from_teacher(hf_name_or_path: str, mode="weight_copy"): 
-
+    # TODO: Llama support
     config = AutoConfig.from_pretrained(hf_name_or_path)
     config.torch_dtype = torch.bfloat16
     tok = AutoTokenizer.from_pretrained(hf_name_or_path)
@@ -57,5 +59,25 @@ def create_student_from_teacher(hf_name_or_path: str, mode="weight_copy"):
         elif mode == "random_init": 
             template_model = Qwen3ForCausalLM(config)
             return template_model, tok
+    elif config.model_type == "llama":
+        template_model = AutoModelForCausalLM.from_pretrained(hf_name_or_path, torch_dtype=torch.bfloat16) 
+        config.num_hidden_layers = len(LAYER_COPY_MAP[template_model.config.num_hidden_layers])
+        if mode == "weight_copy": 
+            embeddings = deepcopy(template_model.model.embed_tokens)
+            layers = nn.ModuleList([deepcopy(template_model.model.layers[i]) for i in LAYER_COPY_MAP[template_model.config.num_hidden_layers]])
+            norm = deepcopy(template_model.model.norm)
+            rotary_emb = deepcopy(template_model.model.rotary_emb)
+            lm_head = deepcopy(template_model.lm_head)
+
+            empty_model = LlamaForCausalLM(config)
+            empty_model.model.embed_tokens = embeddings 
+            empty_model.model.layers = layers 
+            empty_model.model.rotary_emb = rotary_emb 
+            empty_model.model.norm = norm 
+            empty_model.lm_head = lm_head 
+            return empty_model, tok
+        elif mode == "random_init": 
+            empty_model = LlamaForCausalLM(config) 
+            return empty_model, tok
     else: 
         raise NotImplementedError("layer selection is not implemented for the current model")
